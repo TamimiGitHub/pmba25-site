e8cc8aabaf218b1e855bef44b83649018bef0123
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2023-01-13 10:10:41 -0800

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Hello PMBA 2025!</Title>
+      <h1>Stay Tuned!</h1>
       <Counter />
       <p>
         Visit{" "}
