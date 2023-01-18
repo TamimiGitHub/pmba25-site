a221317e334bfbbc5432fd4f9b0173a1ddb7740f
Tamimi <tamimi@solace.com>
2023-01-18 13:26:06 -0800

    add alt

--- src/components/Optin/Optin.js
+++ src/components/Optin/Optin.js
@@ -12,11 +12,11 @@ class Optin extends Component {
       <div className="optin">
         <p>Join the conversation</p>
           <a href="https://chat.whatsapp.com/Dh1s8JAfaPzBjsjeFGtG35">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="50" class="icon"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width="50" class="icon" alt="whatsapp-logo"></img>
           </a>
 
           <a href="https://join.slack.com/t/pmba25/shared_invite/zt-1njryqj8k-qs0kNMqt761mGEexwv3eLQ">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg" width="50"></img>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg" width="50" alt="slack-logo"></img>
           </a>
       </div>
     );
