8fb2ae3ada065fe2527491c8f73c15c72676d4e5
Tamimi <tamimi@solace.com>
2023-01-31 09:58:59 -0800

    update header meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,20 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Album from '../components/Album';
+import {Helmet} from "react-helmet";
 
 export default function Index() {
   return (
     <Container maxWidth="lg">
+      <Helmet>
+        <title>PMBA25</title>
+        <meta name="description" content="PMBA 2025 Elections Campaign"/>
+        <meta property="og:title" content="Updated!!"/>
+        <meta property="og:title" content="PMBA25 Elections"/>
+        <meta property="og:description" content="PMBA 2025 Elections Campaign"/>
+        <meta property="og:image" itemProp="image" content="https://d30mzt1bxg5llt.cloudfront.net/public/uploads/images/_signatoryLogo/UBC-Sauder-Full-Colour-JPEG-web.jpg"/>
+        <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/2097/2097443.png" sizes="16x16" />
+      </Helmet>
       <Box sx={{ 
         pt: 2,
         pr: 4,
